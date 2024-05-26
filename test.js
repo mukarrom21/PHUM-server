9e85e5be464e9b36dd4253d9d01730a3b0c0dd01
mukarrom21 <mukarrom21@gmail.com>
2024-05-26 22:58:18 +0600

    added build command

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 const name = {
-  firstName: "Md",
-  middleName: "Mukarrom",
-  lastName: "Hosain"
+  firstName: 'Md',
+  middleName: 'Mukarrom',
+  lastName: 'Hosain',
 }
 
 const unMuted = {}
 
 for (const [key, value] of Object.entries(name)) {
-  unMuted[`name.${key}`]=value
+  unMuted[`name.${key}`] = value
 }
 
-console.log(unMuted);
+console.log(unMuted)
 /* {
   'name.firstName': 'Md',       
   'name.middleName': 'Mukarrom',
